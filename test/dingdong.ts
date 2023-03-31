52b7559d3a261c49eb1897d5fbcfd5da93e40ba0
NickWang <brotherstyx@gmail.com>
2023-03-31 19:49:51 +0800

    feat: 🎸 dingdong

---
+++ test/dingdong.ts
@@ -0,0 +1,25 @@
+import { PuppetWxkf } from '../src/puppet-wxkf'
+
+const puppet = new PuppetWxkf()
+
+puppet.on('error', (e) => {
+  console.log((e as Error).message)
+})
+
+puppet.on('message', async payload => {
+  console.log('message', payload)
+
+  const messageId = payload.messageId
+
+  const messagePayload = await puppet.messageRawPayloadParser(await puppet.messagePayload(messageId))
+
+  if (messagePayload.text === 'ding') {
+    await puppet.messageSendText(messagePayload.talkerId, 'dong')
+  }
+})
+
+puppet.on('ready', payload => {
+  console.log(payload)
+})
+
+puppet.start()
