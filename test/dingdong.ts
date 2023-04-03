8742e2566d38dfe5e4c7b70d88400e6a0bb4a0a9
NickWang <brotherstyx@gmail.com>
2023-04-03 14:55:37 +0800

    fix: 🐛 dingdong

--- test/dingdong.ts
+++ test/dingdong.ts
@@ -1,11 +1,6 @@
-import { types } from '../src/wechaty-dep'
-import { FileBox } from '../src/filebox-dep'
 import { PuppetWxkf } from '../src/puppet-wxkf'
 
 const puppet = new PuppetWxkf()
-
-const file = FileBox.fromUrl('https://s3.cn-north-1.amazonaws.com.cn/xiaoju-material/public/8f69a694-bbbc-43d3-941e-13a38e3ed899%2F3.jpg')
-// const file = FileBox.fromFile('/Users/myonlystarcn/LocalDocuments/code/work/wxkf/wechaty-puppet-wxkf/test/dingdong.ts')
 
 puppet.on('error', (e) => {
   console.log((e as Error).message)
@@ -23,29 +18,14 @@ puppet.on('message', async payload => {
 
   const messagePayload = await puppet.messagePayload(messageId)
 
-  if (true) {
+  if (messagePayload.text === 'ding') {
     await puppet.messageSendText(messagePayload.talkerId, 'dong')
-    // await puppet.messageSendFile(messagePayload.talkerId, file)
   }
-
-  if (messagePayload.type === types.Message.Image) {
-    const file = await puppet.messageFile(messageId)
-    console.log(file)
-    await file.toFile(undefined, true)
-  }
-
-  if (messagePayload.type === types.Message.MiniProgram) {
-    console.log(await puppet.messageMiniProgram(messageId))
-  }
-
-  const contact = await puppet.contactPayload(messagePayload.talkerId)
-  console.log(contact)
 
 })
 
 puppet.on('ready', payload => {
   console.log(payload)
-  // void puppet.messageSendFile('wmyhmpBwAAgjUIT_sNHgXv0wW7bmRc8Q', file)
 })
 
 puppet.start()
