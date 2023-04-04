6136ec14b4985f68b50817093e1c4eb66a885ffc
NickWang <brotherstyx@gmail.com>
2023-04-04 14:59:46 +0800

    fix: 🐛 env naming

--- src/util/env.ts
+++ src/util/env.ts
@@ -4,13 +4,13 @@ import { WXKF_ERROR } from '../error/error-code'
 import { WxkfAuth } from '../schema/base'
 
 export const getAuthData = (options: WxkfAuth = {}) => {
-  const token = options.token || process.env['WECOM_APP_TOKEN']
+  const token = options.token || process.env['PUPPET_WXKF_WECOM_APP_TOKEN']
   const encodingAESKey =
-    options.encodingAESKey || process.env['WECOM_APP_AES_KEY']
-  const corpId = options.corpId || process.env['WECOM_CORP_ID']
-  const corpSecret = options.corpSecret || process.env['WECOM_CORP_SECRET']
-  const kfOpenId = options.kfOpenId || process.env['WECOM_KF_OPEN_ID']
-  const kfName = options.kfName || process.env['WECOM_KF_NAME']
+    options.encodingAESKey || process.env['PUPPET_WXKF_WECOM_APP_AES_KEY']
+  const corpId = options.corpId || process.env['PUPPET_WXKF_WECOM_CORP_ID']
+  const corpSecret = options.corpSecret || process.env['PUPPET_WXKF_WECOM_CORP_SECRET']
+  const kfOpenId = options.kfOpenId || process.env['PUPPET_WXKF_WECOM_KF_OPEN_ID']
+  const kfName = options.kfName || process.env['PUPPET_WXKF_WECOM_KF_NAME']
 
   const result = {
     token,
@@ -39,12 +39,12 @@ export const getAuthData = (options: WxkfAuth = {}) => {
 }
 
 export const getPort = (portOption: string) => {
-  const port = Number(portOption || process.env['WXKF_CALLBACK_PORT']) || 3000
+  const port = Number(portOption || process.env['PUPPET_WXKF_WXKF_CALLBACK_PORT']) || 3000
   return port
 }
 
 export const getOss = () => {
-  const type = process.env['OSS_CLIENT_TYPE'] || ''
+  const type = process.env['PUPPET_WXKF_OSS_CLIENT_TYPE'] || ''
   if (!type) {
     return {
       ossClientType: '',
@@ -52,11 +52,11 @@ export const getOss = () => {
   }
 
   const basicConfig = {
-    ossClientType: process.env['OSS_CLIENT_TYPE'],
-    ossRegion: process.env['OSS_REGION'],
-    ossAccessKeyId: process.env['OSS_ACCESS_KEY_ID'],
-    ossAccessKeySecret: process.env['OSS_ACCESS_KEY_SECRET'],
-    ossBucket: process.env['OSS_BUCKET'],
+    ossClientType: process.env['PUPPET_WXKF_OSS_CLIENT_TYPE'],
+    ossRegion: process.env['PUPPET_WXKF_OSS_REGION'],
+    ossAccessKeyId: process.env['PUPPET_WXKF_OSS_ACCESS_KEY_ID'],
+    ossAccessKeySecret: process.env['PUPPET_WXKF_OSS_ACCESS_KEY_SECRET'],
+    ossBucket: process.env['PUPPET_WXKF_OSS_BUCKET'],
   }
 
   const ossConfig: OssConfig = {
@@ -64,27 +64,27 @@ export const getOss = () => {
   }
   switch (type) {
     case OSS_CLIENT_TYPE.S3:
-      (ossConfig as OssS3Config).ossExpireTime = Number(process.env['OSS_EXPIRE_TIME'] || 3600 * 1000 * 24 * 30)
+      (ossConfig as OssS3Config).ossExpireTime = Number(process.env['PUPPET_WXKF_OSS_EXPIRE_TIME'] || 3600 * 1000 * 24 * 30)
       break
     case OSS_CLIENT_TYPE.Ali:
-      (ossConfig as OssAliConfig).ossAliInternal = process.env['OSS_ALI_INTERNAL'] === 'true'
+      (ossConfig as OssAliConfig).ossAliInternal = process.env['PUPPET_WXKF_OSS_ALI_INTERNAL'] === 'true'
       break
     case OSS_CLIENT_TYPE.Minio:
-      (ossConfig as OssMinioConfig).ossMinioUseSsl = process.env['OSS_MINIO_USE_SSL'] === 'true'
-      ;(ossConfig as OssMinioConfig).ossMinioEndpoint = process.env['OSS_MINIO_ENDPOINT'] || 'play.min.io'
-      ;(ossConfig as OssMinioConfig).ossMinioPort = Number(process.env['OSS_MINIO_PORT'] || 9000)
-      ;(ossConfig as OssMinioConfig).ossMinioDebugFile = process.env['OSS_MINIO_DEBUG_FILE'] || ''
+      (ossConfig as OssMinioConfig).ossMinioUseSsl = process.env['PUPPET_WXKF_OSS_MINIO_USE_SSL'] === 'true'
+      ;(ossConfig as OssMinioConfig).ossMinioEndpoint = process.env['PUPPET_WXKF_OSS_MINIO_ENDPOINT'] || 'play.min.io'
+      ;(ossConfig as OssMinioConfig).ossMinioPort = Number(process.env['PUPPET_WXKF_OSS_MINIO_PORT'] || 9000)
+      ;(ossConfig as OssMinioConfig).ossMinioDebugFile = process.env['PUPPET_WXKF_OSS_MINIO_DEBUG_FILE'] || ''
       break
     case OSS_CLIENT_TYPE.Cos:
-      (ossConfig as OssCosConfig).ossCosAppid = process.env['OSS_COS_APPID']
+      (ossConfig as OssCosConfig).ossCosAppid = process.env['PUPPET_WXKF_OSS_COS_APPID']
       break
     case OSS_CLIENT_TYPE.Tos:
-      (ossConfig as OssTosConfig).ossBasePath = process.env['OSS_BASE_PATH']
-      ;(ossConfig as OssTosConfig).ossEndpoint = process.env['OSS_ENDPOINT']
-      ;(ossConfig as OssTosConfig).ossSecure = process.env['OSS_SECURE'] === 'true'
+      (ossConfig as OssTosConfig).ossBasePath = process.env['PUPPET_WXKF_OSS_BASE_PATH']
+      ;(ossConfig as OssTosConfig).ossEndpoint = process.env['PUPPET_WXKF_OSS_ENDPOINT']
+      ;(ossConfig as OssTosConfig).ossSecure = process.env['PUPPET_WXKF_OSS_SECURE'] === 'true'
       break
     default:
-      (ossConfig as OssS3Config).ossExpireTime = Number(process.env['OSS_EXPIRE_TIME'] || 3600 * 1000 * 24 * 30)
+      (ossConfig as OssS3Config).ossExpireTime = Number(process.env['PUPPET_WXKF_OSS_EXPIRE_TIME'] || 3600 * 1000 * 24 * 30)
   }
   return ossConfig
 }
